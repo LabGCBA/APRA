b879a22731e0a622b096eb56893bceba13a0368c
Hernán Patrón Costas <hernanpatroncostas@gmail.com>
2016-08-24 13:55:33 -0300

    Agregado Jquery ajax

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'root',
   database : 'APRA'
 });
 
