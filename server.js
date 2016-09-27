e4acd697e936aad142fcc2f7e0e0c3a1098e67b7
Hernán Patrón Costas <hernanpatroncostas@gmail.com>
2016-09-27 15:11:08 -0300

    Mejoras y correcciones varias

--- server.js
+++ server.js
@@ -15,116 +15,6 @@ var estaciones = [];
 var sensores = [];
 var mediciones = [];
 var medicionesayer = [];
-var prom8 = {};
-var prom12 = {};
-cronJob();
-function asyncLoop(iterations, func, callback) {
-    var index = 0;
-    var done = false;
-    var loop = {
-        next: function() {
-            if (done) {
-                return;
-            }
-
-            if (index < iterations) {
-                index++;
-                func(loop);
-
-            } else {
-                done = true;
-                callback();
-            }
-        },
-
-        iteration: function() {
-            return index - 1;
-        },
-
-        break: function() {
-            done = true;
-            callback();
-        }
-    };
-    loop.next();
-    return loop;
-}
-function cronJob() {
-	var now = new Date();
-	listApi("stations", "estaciones", function(){
-		asyncLoop(estaciones.length, function(loop) {
-			value = estaciones[loop.iteration()];
-			prom8[value.Id] = {};
-			prom12[value.Id] = {};
-			listApi("stations/"+value.Id, "sensores", function(){
-				asyncLoop(sensores.length, function(loop2) {
-					value2 = sensores[loop2.iteration()];
-					get = "sensors/" + value.Id + "/" + value2.Id + "/" + now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate();
-					listApi(get, "mediciones", function(){
-						get = "sensors/" + value.Id + "/" + value2.Id + "/" + now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + (now.getDate()-1);
-						listApi(get, "medicionesayer", function(){
-						if (mediciones.length >= 48){
-							sum = 0;
-							for (i = mediciones.length-1; i >= mediciones.length - 48; i--) {
-								sum += mediciones[i].State;
-							}
-							prom12[value.Id][value2.Id] = sum/48;
-							sum = 0;
-							for (i = mediciones.length-1; i >= mediciones.length - 32; i--){
-								sum += mediciones[i].State;
-							}
-							prom8[value.Id][value2.Id] = (sum/32);
-						}
-						else if (mediciones.length >= 32){
-								sum = 0;
-								for (i = mediciones.length-1; i >= mediciones.length - 32; i--){
-									sum += mediciones[i].State;
-								}
-								prom8[value.Id][value2.Id]  = (sum/32);
-								sum = 0;
-								for (i = mediciones.length-1; i >= 0; i--){
-									sum += mediciones[i].State;
-								}
-								for (i = medicionesayer.length-1; i >= medicionesayer.length + mediciones.length - 48; i--){
-									sum += medicionesayer[i].State;
-								}
-								prom12[value.Id][value2.Id]  = (sum/48);
-						}
-						else{
-								sum = 0;
-								for (i = mediciones.length-1; i >= 0; i--){
-									sum += mediciones[i].State;
-								}
-								for (i = medicionesayer.length-1; i >= medicionesayer.length + mediciones.length-48; i--){
-									sum += medicionesayer[i].State;
-								}
-								prom12[value.Id][value2.Id] = (sum/48);
-								sum = 0;
-								for (i = mediciones.length-1; i >= 0; i--){
-									sum += mediciones[i].State;
-								}
-								for (i = medicionesayer.length-1; i >= medicionesayer.length + mediciones.length-32; i--){
-									sum += medicionesayer[i].State;
-								}
-								prom8[value.Id][value2.Id] = (sum/32);
-						}
-						loop2.next();
-						});
-					});
-				}, function(){
-					loop.next();
-				});
-			});
-		}, function(){
-			console.log(prom8);
-			console.log(prom12);
-		});
-	});
-}
-new CronJob('*/15 * * * *', cronJob , function(){
-	console.log(prom8);
-	console.log(prom12);
-}, true, 'America/Los_Angeles');
 
 function listApi(busq, lista, callback){
 	url = "http://bapocbulkserver.azurewebsites.net/api1/";
@@ -159,10 +49,15 @@ app.get('/mediciones/:sensor/:estacion', function(req, res){
 		anio = date[0];
 		mes = date[1];
 		dia = date[2];
+		if (anio==now.getFullYear() && mes == now.getMonth()+1 && dia == now.getDate())
+			today = true;
+		else 
+			today = false;
 		prom = true;
 		if(req.query.details){
 			get = "sensors/"+req.params.estacion+"/"+req.params.sensor + "/detail/" +anio+"/"+mes+"/"+dia;
 			detalles = true;
+			today = false;
 		}
 		else{
 			get = "sensors/"+req.params.estacion+"/"+req.params.sensor + "/" +anio+"/"+mes+"/"+dia;
@@ -171,11 +66,16 @@ app.get('/mediciones/:sensor/:estacion', function(req, res){
 	}
 	else {
 		anio = now.getFullYear();
-		get = "sensors/"+req.params.estacion+"/"+req.params.sensor+"/"+anio;
+		get = "sensors/"+req.params.estacion+"/"+req.params.sensor+"/"+anio+"/"+(now.getMonth()+1);
+		console.log(get);
 		detalles = false;
+		today = false;
 	}
 	listApi(get, "mediciones", function(){
-		data = {
+		if (today){
+			get = "sensors/"+req.params.estacion+"/"+req.params.sensor+"/"+anio+"/"+mes+"/"+(dia-1);
+			listApi(get, "medicionesayer", function(){
+			data = {
 					data : {
 					fecha : req.query.date,
 					details : detalles,
@@ -184,11 +84,31 @@ app.get('/mediciones/:sensor/:estacion', function(req, res){
 					estaciones : estaciones,
 					sensores : sensores,
 					mediciones : mediciones,
-					prom8 : prom8[(req.params.estacion)][req.params.sensor],
-					prom12 : prom12[(req.params.estacion)][req.params.sensor]
+					medicionesayer : medicionesayer,
+					today : true
 					}
-				};
-	 res.render('medicion', data);
+		
+			};
+	 		res.render('medicion', data);
+		});
+		}
+		else {
+			data = {
+					data : {
+					fecha : req.query.date,
+					details : detalles,
+					sensor_id : req.params.sensor,
+					estacion_id : req.params.estacion,
+					estaciones : estaciones,
+					sensores : sensores,
+					mediciones : mediciones,
+					medicionesayer : medicionesayer,
+					today : false
+					}
+		
+			};
+	 		res.render('medicion', data);
+		}
 	});
 });
 
